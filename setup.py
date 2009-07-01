84f590b1ca11d5ab709fcc59fe42cc1fa501c3a8
thales <thales@ninja.gina.alaska.edu>
2009-07-01 13:04:36 -0800

    adding README and setup.py

---
+++ setup.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(
+    name = 'images2cot',
+    version = '1.0',
+    description = 'Images to Cursor on Target XML feed',
+    author = 'Jonathan Sawyer',
+    author_email = 'jon@gina.alaska.edu',
+    url = 'http://gina.alaska.edu/',
+    license = 'MIT License',
+    packages=['images2cot'],
+    package_dir={'mypkg': 'images2cot'},
+    )
